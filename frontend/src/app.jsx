721d23fd570192cb0ffc7ee8b3dbc78f1e0174dd
Cameron Williams <cswilliams112004@gmail.com>
2024-03-13 15:12:06 -0400

    first commit

---
+++ frontend/src/app.jsx
@@ -0,0 +1,8 @@
+import { useState } from "preact/hooks";
+import Routes from "./routes/Routes";
+import Layout from "./pages/Layout";
+import "./app.css";
+
+export function App() {
+  return <Routes />;
+}
