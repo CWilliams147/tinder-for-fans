220a76ae3c0d41789e91eb72474e2a7d44bcb54d
Cameron Williams <cswilliams112004@gmail.com>
2024-03-15 19:05:25 -0400

    future feature but haven't done anything yet

---
+++ frontend/src/routes/Matches.jsx
@@ -0,0 +1,19 @@
+// const url =
+//   "https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=2023";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "1cf21f8c6bmshe9b2f507ab3ff31p1b4effjsn39fb01da858e",
+//     "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+//   },
+// };
+
+// try {
+//   const response = await fetch(url, options);
+//   const result = await response.json();
+//   console.log(result);
+// } catch (error) {
+//   console.error(error);
+// }
+
+// export default Matches;
