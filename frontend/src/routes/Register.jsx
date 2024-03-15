1e4902d25ec764f8843b803f80ed501e6c157bbc
Cameron Williams <cswilliams112004@gmail.com>
2024-03-15 19:06:02 -0400

    added divs for the layout

--- frontend/src/routes/Register.jsx
+++ frontend/src/routes/Register.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useState } from "preact/hooks";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, Link } from "react-router-dom";
+import Nav from "../components/Nav";
 
 export async function action({ request }) {
   const formData = await request.formData();
   const name = formData.get("name");
   const email = formData.get("email");
   const password = formData.get("password");
-  const logindata = { name, email, password };
+  const registerData = { name, email, password };
   try {
     const url = `${import.meta.env.VITE_SOURCE_URL}/register`;
     const options = {
@@ -15,7 +16,7 @@ export async function action({ request }) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(logindata),
+      body: JSON.stringify(registerData),
     };
     console.log(url);
     const response = await fetch(url, options);
@@ -32,22 +33,36 @@ export async function action({ request }) {
 const Register = () => {
   return (
     <>
-      <h2>Register</h2>
-      <Form method="post">
-        <label>
-          Your Name
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Your Email
-          <input type="text" name="email" />
-        </label>
-        <label>
-          Your Password
-          <input type="password" name="password" />
-        </label>
-        <button type="submit">Register User</button>
-      </Form>
+      <div className="onloading-background">
+        <div className="login-form">
+          <h2>Register</h2>
+          <Form method="post">
+            <div className="input-container">
+              <label>
+                Your Name
+                <input type="text" name="name" required />
+              </label>
+            </div>
+            <div className="input-container">
+              <label>
+                Your Email
+                <input type="text" name="email" required />
+              </label>
+            </div>
+            <div className="input-container">
+              <label>
+                Your Password
+                <input type="password" name="password" required />
+              </label>
+            </div>
+            <button type="submit">Register User</button>
+          </Form>
+          <div>------------ or ------------</div>
+          <Link to="/login">
+            <button>Log in Here</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
