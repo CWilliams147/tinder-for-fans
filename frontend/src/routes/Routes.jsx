6f2329411bfe6419f825ba64d2756cbfa2c83f65
Cameron Williams <cswilliams112004@gmail.com>
2024-03-21 16:04:54 -0400

    added the protected routes

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -8,52 +8,59 @@ import ContactPage from "./ContactPage";
 import Layout from "../pages/Layout";
 import Error from "../pages/Error";
 import Home from "./Home";
+import ProtectedRoute from "./ProtectedRoute";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "register",
+        element: <Register />,
+        action: registerAction,
+      },
+      {
+        path: "login",
+        element: <Login />,
+        action: loginAction,
+      },
+      {
+        path: "createprofile",
+        element: <CreateProfile />,
+      },
+      {
+        path: "logout",
+        element: (
+          <ProtectedRoute>
+            <Logout />
+          </ProtectedRoute>
+        ),
+        action: logoutAction,
+      },
+      {
+        path: "contact",
+        element: (
+          <ProtectedRoute>
+            <ContactPage />
+          </ProtectedRoute>
+        ),
+      },
+      // {
+      //   path: "matches",
+      //   element: (
+      //     <ProtectedRoute>
+      //       <Matches />
+      //     </ProtectedRoute>
+      //   ),
+      // },
+    ],
+  },
+]);
 
-const Routes = () => {
-  const publicRoutes = [
-    {
-      element: <Layout />,
-      errorElement: <Error />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/register",
-          element: <Register />,
-          action: registerAction,
-        },
-        {
-          path: "/login",
-          element: <Login />,
-          action: loginAction,
-        },
-        {
-          path: "/logout",
-          element: <Logout />,
-          action: logoutAction,
-        },
-        {
-          path: "/createprofile",
-          element: <CreateProfile />,
-          action: createProfileAction,
-        },
-        // {
-        //   path: "/matches",
-        //   element: <Matches />,
-        // },
-        {
-          path: "/contact",
-          element: <ContactPage />,
-        },
-      ],
-    },
-  ];
-
-  const router = createBrowserRouter([...publicRoutes]);
-
+function Routes() {
   return <RouterProvider router={router} />;
-};
+}
 
 export default Routes;
