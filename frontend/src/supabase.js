fe940a99e034e9521e1b424efafa1c398827733e
Cameron Williams <cswilliams112004@gmail.com>
2024-03-21 16:01:42 -0400

    switched around keys to try and figure out posting issues

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const supabase_url = import.meta.env.VITE_SOURCE_URL;
 const anon_key = import.meta.env.VITE_API_KEY;
-
+// const service_key = import.meta.env.VITE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabase_url, anon_key);
+
 export default supabase;
