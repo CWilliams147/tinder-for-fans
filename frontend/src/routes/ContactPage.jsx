ef0ed06c3e284da71876af8a156611d09f38896a
Cameron Williams <cswilliams112004@gmail.com>
2024-03-26 19:50:17 -0400

    styling

--- frontend/src/routes/ContactPage.jsx
+++ frontend/src/routes/ContactPage.jsx
@@ -22,37 +22,39 @@ const ContactPage = () => {
   return (
     <>
       <Nav />
-      <div className="contact-page-container">
-        <div className="form-container">
-          <h1>Contact Us</h1>
-          <form className="form" onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea
-                name="message"
-                id="message"
-                rows="10"
-                cols="50"
-                value={formData.message}
-                onChange={handleChange}
-                required
-              ></textarea>
-            </div>
-            <button className="form-submit-btn" type="submit">
-              Submit
-            </button>
-          </form>
+      <div className="contact-page-alignment">
+        <div className="contact-page-container">
+          <div className="form-container">
+            <h1>Contact Us</h1>
+            <form className="form" onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  rows="10"
+                  cols="50"
+                  value={formData.message}
+                  onChange={handleChange}
+                  required
+                ></textarea>
+              </div>
+              <button className="form-submit-btn" type="submit">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
