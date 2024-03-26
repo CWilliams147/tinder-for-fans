083cbdbf66c3cba9543ffc9841ea985a2083697c
Cameron Williams <cswilliams112004@gmail.com>
2024-03-26 10:15:57 -0400

    set up a lot of the components on this page

--- frontend/src/routes/ProfileDisplay.jsx
+++ frontend/src/routes/ProfileDisplay.jsx
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from "react";
+import Nav from "../components/Nav";
 import supabase from "../supabase";
 
-const ProfileDisplay = ({ team }) => {
+const ProfileDisplay = () => {
   const [profiles, setProfiles] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const baseUrl =
+    "https://oiqeacnqaafatdeoccav.supabase.co/storage/v1/object/public/profile-photos/";
+
+  const fetchUserTeam = async () => {
+    const userId = localStorage.getItem("user_id");
+    const { data } = await supabase
+      .from("profile")
+      .select("team")
+      .eq("id", userId);
+    const { team } = data[0];
+    return team;
+  };
+
   const fetchProfiles = async () => {
+    const team = await fetchUserTeam();
     const { data, error } = await supabase
       .from("profile")
       .select("*")
       .eq("team", team);
-
-    console.log(data, error);
 
     if (error) {
       console.error("Error fetching profiles:", error.message);
@@ -23,27 +36,34 @@ const ProfileDisplay = ({ team }) => {
 
   useEffect(() => {
     fetchProfiles();
-  }, [team]);
+  }, []);
 
   const handleNextProfile = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % profiles.length);
   };
 
   return (
-    <div>
-      {profiles.length > 0 ? (
-        <>
-          <div>
-            <h2>{profiles[currentIndex].name}</h2>
-            <img src={profiles[currentIndex].profile_picture} alt="Profile" />
-            <p>Team: {profiles[currentIndex].team}</p>
-          </div>
-          <button onClick={handleNextProfile}>Next Profile</button>
-        </>
-      ) : (
-        <p>No profiles found for team {team}.</p>
-      )}
-    </div>
+    <>
+      <Nav />
+      <div>
+        {profiles.length > 0 ? (
+          <>
+            <div>
+              <h2>{profiles[currentIndex].name}</h2>
+              <img
+                className="profile-pic-display"
+                src={`${baseUrl}${profiles[currentIndex].profile_picture}`}
+                alt="Profile"
+              />
+              <div>Team: {profiles[currentIndex].team}</div>
+            </div>
+            <button onClick={handleNextProfile}>Next Profile</button>
+          </>
+        ) : (
+          <p>No profiles found for team.</p>
+        )}
+      </div>
+    </>
   );
 };
 
