7de8df8464e04203a96961457f616ea0e65c6587
Cameron Williams <cswilliams112004@gmail.com>
2024-03-28 16:30:06 -0400

    changed the styling on the home button

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -8,9 +8,13 @@ const Home = () => {
       <div className="page-home">
         <div className="home-title">Connect on the</div>
         <div className="home-title2">Pitch!</div>
-        <div className="home-subtitle">Find your football tribe.</div>
+        <div className="home-subtitle">
+          Connect With Other Fans and Find your football tribe.
+        </div>
         <Link to="/register">
-          <button className="home-button">Join the League</button>
+          <button className="home-button">
+            <span>Join the League</span>
+          </button>
         </Link>
         <img
           src="https://oiqeacnqaafatdeoccav.supabase.co/storage/v1/object/public/home-image/home"
